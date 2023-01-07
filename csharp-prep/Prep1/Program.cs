91129bda32769a4dff5412f060059c9a94553779
Otavio Valeriano <otaviovalerianoss@gmail.com>
2023-01-07 00:24:48 -0700

    Prep 1 & 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Inform your First Name: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Inform your Last Name: ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("Your name is " + lastName + ", " + firstName + " " + lastName);
+        // Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write(" Inform your grade in %: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+            else if (grade >= 80 ) 
+            {
+                letter = "B";
+            }
+            else if (grade >= 70 ) {
+                letter = "C";
+            }
+            else if (grade >= 60 ) {
+                letter = "D";
+            }
+            else if (grade >= 50)
+            {
+            letter = "F";
+            }
+        
+        Console.WriteLine($"Your Grade is: {letter} !!");
+
+        if (grade > 70) {
+            Console.Write("Congratulations, You Passed!!");
+        }
+        else {
+            Console.Write("You Failed - Good Luck Next Time!!");
+        }
+
     }
 }
