4aa1f216125659aebabb9392475923fff22512cc
Otavio Valeriano <otaviovalerianoss@gmail.com>
2023-01-11 10:40:41 -0700

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+        // Console.WriteLine($" This is your number: {number} ");
+
+        bool guessed = false;
+        int turns = 0;
+        while (guessed == false)
+        {
+            Console.Write("Please enter your guess: ");
+            int answer = int.Parse(Console.ReadLine());
+            turns += 1;
+
+            if (answer > number) 
+            {
+                Console.WriteLine("lower");
+            }
+            else if (answer < number)
+            {
+                Console.WriteLine("higher");
+            }
+            else 
+            {
+                Console.WriteLine("Correct!!! Great Job");
+            // Console.WriteLine($"They guessed {answer}");
+                guessed = true;                
+            }
+        }
+        Console.WriteLine($"It took you {turns} turns to guess the righ number!");
+
     }
 }
