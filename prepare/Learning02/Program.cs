31adb83acab3fa59cb8744a5305dc91416b33bb4
Otavio Valeriano <otaviovalerianoss@gmail.com>
2023-01-18 10:45:48 -0700

    Learning2 - prep activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+       Job job1 = new Job();
+       job1._jobTitle = "Software Engineer";
+       job1._company = "Google";
+       job1._startYear = 2000;
+       job1._endYear = 2010;
+       
+       Job job2 = new Job();
+       job2._jobTitle = "Web Developer";
+       job2._company = "Reddit";
+       job2._startYear = 2021;
+       job2._endYear = 2023;
+
+       Resume myResume = new Resume();
+       myResume._name = "Otavio Silva";
+       myResume._jobs.Add(job1);
+       myResume._jobs.Add(job2);
+       
+       myResume.Display();
     }
+
 }
