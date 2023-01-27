7ca168fffbe3c4e3a7585d6a29209a3f67db59eb
Otavio Valeriano <otaviovalerianoss@gmail.com>
2023-01-27 12:53:18 -0700

    Journal progress. Menu done. Entry 90%. Disp 60%

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,29 +1,48 @@
-public class Entry {
+public class Entry
+{
 
     public List<string> _entries = new List<string>();
     public List<string> _prompts = new List<string>();
+    DateTime _date = DateTime.Today;
 
-    public Entry(){
+    public Entry()
+    {
 
     }
 
-    public void GeneratePrompts(){
+    public void GeneratePrompts()
+    {
         _prompts.Add("What happened to you today?: ");
         _prompts.Add("Write a fun thing you did today: ");
         _prompts.Add("Write a sad that happened to you this week: ");
         _prompts.Add("Tell me a fun fact about you: ");
         _prompts.Add("What are you grateful for today?: ");
-        // i need to make it random for the option of prompts.
-        Console.WriteLine(_prompts[0]);
     }
-    public void InsertEntry(){        
-        // Console.Write("What happened to you today?");
-        _entries.Add(Console.ReadLine());
-        Console.WriteLine(_entries[0]);
-
-        
+    public void InsertEntry()
+    {
+        //  random for the option of prompts.
+        Random numGenerator = new Random();
+        int index = numGenerator.Next(4);
+        string randomQuestion = (_prompts[index]);
+        Console.Write(randomQuestion);
+        string answer = Console.ReadLine();
+        string entryDate = _date.ToString("MM/dd/yyyy");
+        string wholeEntry = ($"{entryDate} - {randomQuestion} - {answer}");
+        _entries.Add(wholeEntry);
+        // the list is saving here, but replacing the last adition.
+        // for (int i = 0; i < _entries.Count; i++)
+        // {
+        //     Console.WriteLine(_entries[i]);
+        // } 
     }
 
-
-
+    public void displayPrompt()
+    {
+        //solve the fact that the list _entries is not saving globally.
+        for (int i = 0; i < _entries.Count; i++)
+        {
+            Console.WriteLine(_entries[i]);
+        }
+        Console.WriteLine("its working");
+    }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,14 +1,72 @@
 using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //new object with the class Entry - to call methods.
-        Entry p = new Entry();
-        // generates one of the prompts.
-        p.GeneratePrompts();
-        p.InsertEntry();
-        
+        Menu();
+
+        static void Menu()
+        {
+            Entry p = new Entry();
+            int choice = 0;
+            // Console.Clear();
+            Console.WriteLine("Please select one of the following choices: ");
+            Console.WriteLine(" 1. Write \n 2. Display \n 3. Load \n 4. Save \n 5. Quit");
+            choice = int.Parse(Console.ReadLine());
+            if (choice == 5)
+            {
+                Console.Write("See yah!");
+            }
+            else
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("option 1");
+                        //new object with the class Entry - to call methods.
+                        // generates one of the prompts.
+                        p.GeneratePrompts();
+                        p.InsertEntry();
+
+                        // returns to menu after adding the entry.
+                        Menu();
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine("option 2");
+                        p.displayPrompt();
+                        // Menu();
+
+                        break;
+
+                    case 3:
+                        Console.WriteLine("option 3");
+                        Menu();
+
+                        break;
+
+                    case 4:
+                        Console.WriteLine("option 4");
+                        Menu();
+
+                        break;
+                    default:
+                        //if a number other than 1-5 is entered, request
+                        //player enter a number in that range
+                        //wait for them to press enter, then call
+                        //the menu again
+                        Console.WriteLine("Please enter a number 1-5.");
+                        Console.WriteLine("Press enter to continue...");
+                        Console.ReadLine();
+                        Menu();
+                        break;
+                }
+
+            }
+
+        }
     }
 }
