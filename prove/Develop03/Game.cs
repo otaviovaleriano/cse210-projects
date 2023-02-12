e7be2b15aa1e571adef9186a298119cba47480ce
Otavio Valeriano <otaviovalerianoss@gmail.com>
2023-02-11 21:27:34 -0700

    adding feature to hide only showing words & verify

--- prove/Develop03/Game.cs
+++ prove/Develop03/Game.cs
@@ -1,62 +1,33 @@
 class Memorize
 {
     private Scripture scripture;
-    private Random random;
 
     public Memorize(Scripture scripture)
     {
         this.scripture = scripture;
-        random = new Random();
     }
 
     public void Start()
-    {
-        
-        // Console.WriteLine(GetDisplayText());
+    {       
+
         while (true)
         {
             Console.Clear();
-            Console.WriteLine(scripture.Reference);
-            Console.WriteLine(GetDisplayText());
-            Console.WriteLine("Press enter to continue or type 'quit' to finish: ");            
-            string answer = Console.ReadLine();
-            if (answer == "quit")
+            Console.WriteLine(scripture.Reference);            
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Press enter to hide more words, or type 'quit' to exit");
+            string input = Console.ReadLine();
+            if (input == "quit")
             {
                 break;
             }
-
-            HideRandomWords();
-            // Console.Clear();
-            // Console.WriteLine(scripture.Reference);
-            // Console.WriteLine(GetDisplayText());
-        }
-
-    }
-
-    private void HideRandomWords()
-    {
-        int count = random.Next(1, scripture.Words.Count);
-        for (int i = 0; i < count; i++)
-        {
-            int index = random.Next(0, scripture.Words.Count);
-            scripture.Words[index].Hidden = true;
-        }
-    }
-
-    private string GetDisplayText()
-    {
-        string displayText = "";
-        foreach (Word word in scripture.Words)
-        {
-            if (word.Hidden)
+            scripture.HideRandomWords();
+            if (scripture.HiddenWords.All(x => x))
             {
-                displayText += "_ ";
-            }
-            else
-            {
-                displayText += word.Text + " ";
+                Console.Clear();
+                Console.WriteLine("All words have been hidden!");
+                break;
             }
         }
-        return displayText;
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,18 +1,49 @@
-  class Scripture
-    {
-        public string Reference { get; set; }
-        public string Text { get; set; }
-        public List<Word> Words { get; set; }
+class Scripture
+{
+    public string Reference { get; set; }
+    public string Text { get; set; }
+    public bool[] HiddenWords { get; set; }
 
-        public Scripture(string reference, string text)
+    public Scripture(string reference, string text)
+    {
+        Reference = reference;
+        Text = text;
+        HiddenWords = new bool[text.Split(' ').Length];
+    }
+
+    public void HideRandomWords()
+    {
+        Random random = new Random();
+        int hiddenCount = 0;
+        for (int i = 0; i < HiddenWords.Length; i++)
         {
-            Reference = reference;
-            Text = text;
-            Words = new List<Word>();
-            string[] words = text.Split(' ');
-            foreach (string word in words)
+            if (!HiddenWords[i] && random.NextDouble() < 0.5)
             {
-                Words.Add(new Word(word));
+                HiddenWords[i] = true;
+                hiddenCount++;
             }
         }
+        if (hiddenCount == 0)
+        {
+            HiddenWords[random.Next(HiddenWords.Length)] = true;
+        }
     }
+
+    public string GetDisplayText()
+    {
+        string[] words = Text.Split(' ');
+        string displayText = "";
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (HiddenWords[i])
+            {
+                displayText += "_____ ";
+            }
+            else
+            {
+                displayText += words[i] + " ";
+            }
+        }
+        return displayText;
+    }
+}
